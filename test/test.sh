7d6bcd93cc09b0c2da5a121947ecf3d9906105db
John <john.zheng@hp.com>
2017-06-30 10:51:01 +0000

    changes for aws

--- test/test.sh
+++ test/test.sh
@@ -1,22 +1,22 @@
 
 # List topics
-kubectl exec testclient -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
+kubectl exec  -n kafka testclient  -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
 
 # Create topic
-kubectl exec testclient -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --topic test1 --create --partitions 1 --replication-factor 1
+kubectl exec  -n kafka testclient  -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --topic test1 --create --partitions 1 --replication-factor 1
 
 # Set one of your terminals to listen to messages on the test topic
-kubectl exec -ti testclient -- ./bin/kafka-console-consumer.sh --zookeeper zookeeper:2181 --topic test1 --from-beginning
+kubectl exec -ti  -n kafka testclient  -- ./bin/kafka-console-consumer.sh --zookeeper zookeeper:2181 --topic test1 --from-beginning
 
 # Go ahead and produce messages
 echo "Write a message followed by enter, exit using Ctrl+C"
-kubectl exec -ti testclient -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker.kafka.svc.cluster.local:9092 --topic test1
+kubectl exec -ti  -n kafka testclient  -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker.kafka.svc.cluster.local:9092 --topic test1
 
 # Bootstrap even if two nodes are down (shorter name requires same namespace)
-kubectl exec -ti testclient -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker:9092,kafka-1.broker:9092,kafka-2.broker:9092 --topic test1
+kubectl exec -ti  -n kafka testclient  -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker:9092,kafka-1.broker:9092,kafka-2.broker:9092 --topic test1
 
 # The following commands run in the pod
-kubectl exec -ti testclient -- /bin/bash
+kubectl exec -ti  -n kafka testclient  -- /bin/bash
 
 # Topic 2, replicated
 ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --describe --topic test2
